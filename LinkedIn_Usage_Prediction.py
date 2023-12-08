21f93586a18b0394556de3fe42a7f7c0e0ca3b35
Eva-data1013 <153321131+Eva-data1013@users.noreply.github.com>
2023-12-08 18:13:13 -0500

    Update LinkedIn_Usage_Prediction.py

--- LinkedIn_Usage_Prediction.py
+++ LinkedIn_Usage_Prediction.py
@@ -3,8 +3,8 @@ import numpy as np
 import pandas as pd
 import joblib
 
-# Load the trained logistic regression model
-lr_model = joblib.load('trained_lr_model.joblib')  # Update the path if needed
+# Function to preprocess the input data
+lr_model = joblib.load('trained_lr_model.joblib')  
 
 # Function to clean data as per the provided script
 def clean_sm(x):
@@ -12,15 +12,15 @@ def clean_sm(x):
 
 # Function to preprocess the input data
 def preprocess_data(input_data):
-    ss = pd.DataFrame({
-        "income": np.where(input_data["income"] > 9, np.nan, input_data["income"]),
-        "education": np.where(input_data["education"] > 8, np.nan, input_data["education"]),
-        "parent": clean_sm(input_data["parent"]),
-        "married": np.where(input_data["married"] == 1, 1, 0),
-        "female": np.where(input_data["female"] == 2, 1, np.where(input_data["female"] == 1, 0, np.nan)),
-        "age": np.where(input_data["age"] > 98, np.nan, input_data["age"])
-    })
-    return ss.dropna()
+    input_data['income'] = np.where(input_data['income'].isin([10, 11]), -1, input_data['income'])
+    input_data['education'] = np.where(input_data['education'] == 10, -1, input_data['education'])
+    input_data['female'] = np.where(input_data['female'] == 3, -1, 
+                                    np.where(input_data['female'] == 2, 1, 
+                                             np.where(input_data['female'] == 1, 0, -1)))
+    input_data['parent'] = clean_sm(input_data['parent'])
+    input_data['married'] = np.where(input_data['married'] == 1, 1, 0)
+    input_data['age'] = np.where(input_data['age'] > 98, np.nan, input_data['age'])
+    return input_data
 
 # Streamlit app
 st.title('LinkedIn Usage Prediction')
@@ -31,31 +31,30 @@ income_options = [
     '40 to under $50,000', '50 to under $75,000', '75 to under $100,000', '100 to under $150,000',
     '$150,000 or more', 'Don\'t know', 'Refused'
 ]
-income = st.selectbox('Income', options=income_options, index=0)
-income = income_options.index(income) + 1  # Convert to numeric value
+income = st.selectbox('Household Income:', options=income_options, index=0)
+income = income_options.index(income) + 1  
 
 education_options = [
     'Less than high school', 'High school incomplete', 'High school graduate', 'Some college, no degree',
     'Two-year associate degree', 'Four-year college/university degree', 'Some postgraduate or professional schooling',
     'Postgraduate or professional degree', 'Don\'t know', 'Refused'
 ]
-education = st.selectbox('Education', options=education_options, index=0)
-education = education_options.index(education) + 1  # Convert to numeric value
+education = st.selectbox('Highest Education Level:', options=education_options, index=0)
+education = education_options.index(education) + 1  
 
-# Updated parent status options
 parent_options = ['Yes', 'No', 'Don\'t know', 'Refused']
-parent = st.selectbox('Are you a parent of a child under 18 living in your home?', options=parent_options, index=0)
+parent = st.selectbox('Do You Have Children Under 18 at Home?', options=parent_options, index=0)
 parent = parent_options.index(parent) + 1
 
 married_status = ['Married', 'Living with a partner', 'Divorced', 'Separated', 'Widowed', 'Never been married', 'Don\'t know', 'Refused']
-married = st.selectbox('Marital Status', options=married_status, index=0)
+married = st.selectbox('Marital Status:', options=married_status, index=0)
 married = 1 if married_status.index(married) == 0 else 0
 
 gender_options = ['Male', 'Female', 'Other', 'Don\'t know', 'Refused']
-gender = st.selectbox('Gender', options=gender_options, index=0)
+gender = st.selectbox('Gender:', options=gender_options, index=0)
 gender = gender_options.index(gender) + 1
-  
-age = st.slider('Age', 18, 97, 18)
+
+age = st.number_input('Your Age:', min_value=18, max_value=97, value=18, step=1)
 
 # Predict button
 if st.button('Predict LinkedIn Usage'):
@@ -68,7 +67,7 @@ if st.button('Predict LinkedIn Usage'):
         st.write("Invalid input data.")
     else:
         prediction = lr_model.predict(processed_data)
-        prediction_probability = lr_model.predict_proba(processed_data)[0][1]  # Probability of being a LinkedIn user
+        prediction_probability = lr_model.predict_proba(processed_data)[0][1]  
         prediction_text = 'Uses LinkedIn' if prediction[0] == 1 else 'Does not use LinkedIn'
         st.write(f'Prediction: {prediction_text}')
         st.write(f'Probability of using LinkedIn: {prediction_probability:.2f}%')
